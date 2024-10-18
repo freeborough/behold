d616f05ba6b49707726171e097335cdabe8ccd90
Andy Freeborough <andy@freeborough.com>
2024-10-18 18:42:41 +0100

    Suppressed console.error in a errorToIssue test that writes to console.error.

--- packages/server/src/util/results.test.ts
+++ packages/server/src/util/results.test.ts
@@ -1,10 +1,13 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { errorToIssue } from "./results";
 
 describe("util/results", () => {
     describe("errorToIssue", () => {
         it("returns an internal issue if it encounters an error it doesn't explicitly handle", () => {
+            // Mock console.error to supress the errors that will of course be output.
+            vi.spyOn(console, "error").mockImplementation(() => undefined)
             const issues = errorToIssue(new Error("Unknown Error"))
+            vi.restoreAllMocks()
 
             expect(issues[0].kind).toBe("INTERNAL")
         })
