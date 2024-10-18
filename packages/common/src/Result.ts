5c1cce3dc086875e8e2e301763157b93666a8748
Andy Freeborough <andy@freeborough.com>
2024-10-18 19:54:33 +0100

    Created two helper functions, ok and issue, to create Results.
    Just makes things that bit less verbose and ensures that it's all done correctly rathe than just returning random objects.

--- packages/common/src/Result.ts
+++ packages/common/src/Result.ts
@@ -21,6 +21,36 @@ import { Issue } from "./Issue";
  *   }
  * }
  */
-export type Result<T, I = Issue[]> = 
+export type Result<T, I = Issue> = 
     | { ok: true, value: T }
-    | { ok: false, issues: I }
+    | { ok: false, issues: I[] }
+
+/**
+ * Helper function to create an ok Result.
+ * 
+ * @param value The value to be wrapped.
+ * @returns The value wrapped in a result with ok set to true and value set to the passed value.
+ * 
+ * @example
+ * return ok(record)
+ */
+export function ok<T>(value: T) {
+    return { ok: true, value} as Result<T>
+}
+
+/**
+ * Helper function to create an issue Result.
+ * 
+ * @param issues The issues to be wrapped.
+ * @returns The issues wrapped in a result with ok set to false and issues set to the passed issues.
+ * 
+ * @example
+ * return issue(new Issue("Record not found.", IssueKind.CONSTRAINT, "id"))
+ */
+export function issue<T, I = Issue>(issues: I | I[]) {
+    if (!Array.isArray(issues)) {
+        issues = [issues]
+    }
+
+    return { ok: false, issues } as Result<T>
+}
