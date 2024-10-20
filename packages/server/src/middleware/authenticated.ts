883708418d23437e677e7d47f866b4bb489efd1e
Andy Freeborough <andy@freeborough.com>
2024-10-20 09:27:31 +0100

    Refactored isAuthenticated to use the http-status-codes, I missed it when I made that earlier refactor.

--- packages/server/src/middleware/authenticated.ts
+++ packages/server/src/middleware/authenticated.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express"
+import { StatusCodes as HttpStatus } from "http-status-codes"
 
 export function isAuthenticated(request: Request, response: Response, next: NextFunction) {
     if (request.session.user !== undefined) {
         next()
     } else {
-        response.status(403).json()
+        response.status(HttpStatus.FORBIDDEN).send()
     }
 }
