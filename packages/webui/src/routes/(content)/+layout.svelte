bdff484c8e37b86767835a5831cfd9e4bac0e649
Andy Freeborough <andy@freeborough.com>
2024-10-26 13:01:39 +0100

    Reorganised routes into groups so we can separate out the playing game route from the "content" routes for styling purposes.
    - Added placeholder page for /play/[slug] to test the above.  This doesn't include the content div, so there is no margin/limits, as we'll want to use the full space to render our main UI.
    
    - Added an additional layout in the (content) group which adds a wrapping div with the content style.  We use this to then add margins and fix the width of the content so it doesn't spread too far in widescreen making it hard to read.

---
+++ packages/webui/src/routes/(content)/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    const { children } = $props()
+</script>
+<div class="content">
+    {@render children()}
+</div>
---
+++ packages/webui/src/routes/(play)/play/[slug]/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">   
+    import { page } from "$app/stores";
+</script>
+<h1>DEBUG: Playing Game: {$page.params.slug}</h1>
