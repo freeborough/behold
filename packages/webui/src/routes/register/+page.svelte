851870b50b4cd625ff2d0111d361b42361122fe4
Andy Freeborough <andy@freeborough.com>
2024-10-26 11:37:45 +0100

    Moved the contents of the Register component to the register page - as it is a page not a component really.

--- packages/webui/src/routes/register/+page.svelte
+++ packages/webui/src/routes/register/+page.svelte
@@ -1,5 +1,65 @@
 <script lang="ts">
-    import Register from "$lib/ui/Register.svelte"
-</script>
+    import { session } from "$lib/store.svelte"
+    import Dialog from "$lib/ui/Dialog.svelte"
+    import TextField from "$lib/ui/TextField.svelte"
+    import Buttons from "$lib/ui/Buttons.svelte"
+    import { Issue, type UserRegisterClient } from "common"
+    import { UserClient } from "$lib/rest/UserClient"
+    
+    let issues: Issue[] = $state([])
 
-<Register />
+    const registerForm: UserRegisterClient = {
+        username: "", name: "", password: "", passwordConfirmation: ""
+    }
+
+    async function register() {
+        const result = await UserClient.register(registerForm)
+        if (result.ok) {
+            session.user = result.value
+        } else {
+            issues = result.issues
+        }
+    }
+</script>
+<Dialog title="Register">
+    <form>
+        <TextField
+            name="username"
+            label="Email"
+            type="email"
+            placeholder="your@email.com"
+            required={true}
+            {issues}
+            bind:value={registerForm.username} />
+
+        <TextField
+            name="name"
+            label="Display Name"
+            placeholder="Your Display Name"
+            required={true}
+            {issues}
+            bind:value={registerForm.name} />
+        
+        <TextField
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="********"
+            required={true}
+            {issues}
+            bind:value={registerForm.password} />
+        
+        <TextField
+            name="passwordConfirmation"
+            label="Confirm Password"
+            type="password"
+            placeholder="********"
+            required={true}
+            {issues}
+            bind:value={registerForm.passwordConfirmation} />
+
+        <Buttons>
+            <button onclick={register}>Register</button>
+        </Buttons>
+    </form>
+</Dialog>
