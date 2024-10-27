b9c49a4d96b866bc95fda7c3414bcb3681f9785f
Andy Freeborough <andy@freeborough.com>
2024-10-27 10:47:24 +0000

    Moved the /play page into the (play) group as we'll want to protect that route based on authentication.

---
+++ packages/webui/src/routes/(play)/play/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    
+</script>
+
+<div class="content">
+    <h1>Play</h1>
+    <p>Here you can create a new game or play a game you've already created or are a member of.</p>
+    <button>Create New Game</button>
+</div>
