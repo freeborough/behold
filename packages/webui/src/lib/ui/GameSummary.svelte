2b84abcf7119bccf63163b68ab3ba64f2eb96bf7
Andy Freeborough <andy@freeborough.com>
2024-10-28 18:36:47 +0000

    Started work on creating a Games interface - INCOMPLETE.
    - Created a GameSummary component that displays a summary of a game, as the name suggests.
    
    - Expanded the /play page to display the buttons to create a game and enter an invite code, as well as all the games the player is involved in.
    
    - A GameSummary component will display buttons appropriate to the users relationship to the game (e.g. do they own it or not).
    
    NOTE: Currently using dummy data, need to refine the rest client code a little first before implementing the Game client, and need to finish the clientSession cookie bits now we have a use for it.

---
+++ packages/webui/src/lib/ui/GameSummary.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { type Game, type User } from "common"
+    import { goto } from "$lib/util/navigation"
+    import Card from "$lib/ui/Card.svelte"
+    import Buttons from "$lib/ui/Buttons.svelte"
+
+    // DEBUG: Dummy data.
+    // TODO: Get from clientSession cookie (create a function to do so).
+    const user: User = {
+        id: "123",
+        name: "Bilbo Baggins",
+        username: "bilbo@baggins.com",
+    }
+
+    type GameSummaryProps = {
+        game: Game
+    }
+
+    const { game }: GameSummaryProps = $props()
+
+    function join() {
+        goto(`/play/${game.slug}/`)
+    }
+</script>
+<div class="game">
+    <Card>
+        <h2>{game.name}</h2>
+        <p>{game.description}</p>
+        <Buttons>
+            {#if game.owner_id === user.id}
+                <button class="secondary">Delete</button>
+            {:else}
+                <button class="secondary">Withdraw</button>
+            {/if}
+            <button onclick={join}>Join</button>
+        </Buttons>
+    </Card>
+</div>
--- packages/webui/src/routes/(play)/play/+page.svelte
+++ packages/webui/src/routes/(play)/play/+page.svelte
@@ -1,9 +1,59 @@
 <script lang="ts">
-    
+    import Buttons from "$lib/ui/Buttons.svelte"
+    import GameSummary from "$lib/ui/GameSummary.svelte"
+    import type { Game } from "common";
+
+    // DEBUG: Dummy data.
+    const games: Game[] = [
+        {
+            id: "123",
+            owner_id: "123",
+            name: "The Blind Leading the Blind",
+            slug: "the-blind-leading-the-blind",
+            description: "In a world where magic has been forgotten... four wizards set out to save the world.",
+        },
+        {
+            id: "345",
+            owner_id: "345",
+            name: "Mech Assault Mayhem",
+            slug: "mech-assault-mayhem",
+            description: "Four pilots must solve mysterys by punching their way to the truth in giant death robots.",
+        },
+        {
+            id: "123",
+            owner_id: "123",
+            name: "The Blind Leading the Blind",
+            slug: "the-blind-leading-the-blind",
+            description: "In a world where magic has been forgotten... four wizards set out to save the world.",
+        },
+        {
+            id: "123",
+            owner_id: "123",
+            name: "The Blind Leading the Blind",
+            slug: "the-blind-leading-the-blind",
+            description: "In a world where magic has been forgotten... four wizards set out to save the world.",
+        },
+        {
+            id: "345",
+            owner_id: "345",
+            name: "Mech Assault Mayhem",
+            slug: "mech-assault-mayhem",
+            description: "Four pilots must solve mysterys by punching their way to the truth in giant death robots.",
+        },
+    ]
 </script>
 
 <div class="content">
     <h1>Play</h1>
-    <p>Here you can create a new game or play a game you've already created or are a member of.</p>
-    <button>Create New Game</button>
+    <Buttons>
+        <button>Create New Game</button>
+        <button class="secondary">Enter Invite Code</button>
+    </Buttons>
+    {#if games.length > 0}
+        <div class="games">
+            {#each games as game}
+                <GameSummary {game} />
+            {/each}
+        </div>
+    {/if}
 </div>
