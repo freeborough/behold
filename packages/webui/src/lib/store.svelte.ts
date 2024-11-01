465820dcc9f69eeab5797b1ed23c694a0dc41553
Andy Freeborough <andy@freeborough.com>
2024-11-01 19:20:54 +0000

    Started work on monitoring for cookie changes.  - INCOMPLETE
    I'm not entirely convinced about this approach.  Using a polyfil for an API that only has ~75% browser usage at the moment.
    
    Might be better to just monitor the headers of the responses to our requests, but we'll see if this works nicely or not first!

--- packages/webui/src/lib/store.svelte.ts
+++ packages/webui/src/lib/store.svelte.ts
@@ -1,7 +1,14 @@
+import "cookie-store"
 import { type User } from "common"
 
 export type Session = {
     user?: User
 }
+
+window.cookieStore.addEventListner("change", ({ changed }) => {
+    for (const { name, value } of changed) {
+        console.log(`${name} was set to ${value}`)
+    }
+})
 
 export const session: Session = $state({})
